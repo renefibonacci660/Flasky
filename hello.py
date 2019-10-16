6c942497f6cac04aac7509d0176c00ecae657783
renefibonacci660 <660@holbertonschool.com>
2019-10-16 14:45:30 -0700

    Added custom error pages for 500 & 404

--- hello.py
+++ hello.py
@@ -27,3 +27,10 @@ def get_user(id):
         abort(404)
     return '<h1>Hello, {}!</h1>'.format(user.name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
