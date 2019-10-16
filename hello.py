0c796a49e7fe562af2f335e337a467cb47fdfa6a
renefibonacci660 <660@holbertonschool.com>
2019-10-15 19:27:50 -0700

    Added redirect route and decorator for obtaining user ids

--- hello.py
+++ hello.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, redirect, abort
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/bassets')
+def bassets():
+    return redirect('https://www.youtube.com/watch?v=Thb0NHX7jc8&feature=youtu.be')
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/users/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, {}!</h1>'.format(user.name)
+
