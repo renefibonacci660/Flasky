f190ef5a86ecb9d11d4b94c235857eb188c286bd
renefibonacci660 <660@holbertonschool.com>
2019-10-17 18:38:59 -0700

    Implemented user sessions & GET POST

--- hello.py
+++ hello.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 
-from flask import Flask, redirect, abort, render_template
+from flask import Flask, redirect, abort, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string that will be saved as environ var later for added security from CSRF attacks'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route('/bassets')
 def bassets():
